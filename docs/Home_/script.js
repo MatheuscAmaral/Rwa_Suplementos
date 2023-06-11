ca7552bf76df76a47214403adfff5cdca1db8c36
Matheus2 <amaralzgamer321@gmail.com>
2023-06-11 11:35:09 -0300

    .

--- docs/Home_/script.js
+++ docs/Home_/script.js
@@ -1,6 +1,6 @@
 if (localStorage.getItem("token") == null) {
   alert("Você precisa estar logado para acessar essa página");
-  window.location.href = "/pages/Login/index.html";
+  window.location.href = "/docs/Login/index.html";
 }
 
 const userLogado = JSON.parse(localStorage.getItem("userLogado"));
@@ -11,5 +11,5 @@ logado.innerHTML = `${userLogado.nome}`;
 function sair() {
   localStorage.removeItem("token");
   localStorage.removeItem("userLogado");
-  window.location.href = "/pages/Tela_Principal/Index.html";
+  window.location.href = "/docs/Tela_Principal/Index.html";
 }
--- docs/Tela_Principal/sugestões.js
+++ docs/Tela_Principal/sugestões.js
@@ -1,24 +1,123 @@
-let sugestoes = [
-    "Whey",
-    "Whey Pro Max Titanium",
-    "Whey Protein Concentrado Dux",
-    "Whey Isolate Definition Body Action",
-    "Whey 100% Integral Médica",
-    "100% Whey Max Titanium",
-    "Isolate Prime Whey Body Action",
-    "100% Whey Body Nutri",
-    "100% Whey Body Action",
-    "Creatina",
-    "Creatine HD Muscle Hd",
-    "Creatine Turbo Black Skull",
-    "Creatine 5.0 Muscle HD",
-    "Creatine Body Nutri",
-    "Creatine Max Titanium",
-    "Creatine Integral Médica",
-    "Pré-Treino",
-    "Pré-Treino No Control",
-    "Zma Black Skull",
-    "Ripzzz Black Skull",
+const products = [
+    {
+        id: 1,
+        title: 'Whey Pro Max Titanium',
+        price: 120.0,
+        poster: '/docs/Tela_Principal/Imagens/whey-pro-max.png'
+    },
+    
+    {
+        id: 2,
+        title: 'Whey Protein Concentrado Dux',
+        price: 120.0,
+        poster: '/docs/Tela_Principal/Imagens/logo dux.png'
+    },
+
+    {
+        id: 3,
+        title: 'Whey Isolate Definition Body Action',
+        price: 120.0,
+        poster: '/docs/Tela_Principal/Imagens/isolate_definition_bopy-action-.png'
+    },
+
+    {
+        id: 4,
+        title: 'Whey 100% Integral Médica',
+        price: 120.0,
+        poster:'/docs/Tela_Principal/Imagens/whey-100-pouch-900g-baunilha-integralmedica.png'
+    },
+
+    {
+        id: 5,
+        title: '100% Whey Max Titanium',
+        price: 120.0,
+        poster: '/docs/Tela_Principal/Imagens/100 whey max.png'
+    },
+
+    {
+        id: 6,
+        title: 'Isolate Prime Whey Body Action',
+        price: 120.0,
+        poster: '/docs/Tela_Principal/Imagens/isolate_prime_whey_bodyaction.png'
+    },
+
+    {
+        id: 7,
+        title: '100% Whey Body Nutri',
+        price: 120.0,
+        poster: '/docs/Tela_Principal/Imagens/100_-whey-body-nutri-.png'
+    },
+
+    {
+        id: 8,
+        title: '100% Whey Body Action',
+        price: 120.0,
+        poster: '/docs/Tela_Principal/Imagens/100 whey body action.png'
+    },
+
+    {
+        id: 9,
+        title: 'Creatine HD Muscle Hd',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/creatina hd muscle hd.png'
+    },
+
+    {
+        id: 10,
+        title: 'Creatine Turbo Black Skull',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/creatine_black_skull-.png'
+    },
+
+    {
+        id: 11,
+        title: 'Creatine 5.0 Muscle HD',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/creatina 5.0 muscle hd.png'
+    },
+    
+    {
+        id: 12,
+        title: 'Creatine Body Nutri 300g',
+        price: 99.90,
+        poster: '/docs/Tela_Principal/Imagens/creatine double force.png'
+    },
+    
+    {
+        id: 13,
+        title: 'Creatine Max Titanium',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/creatina-150g-max-titanium.png'
+    },
+
+    {
+        id: 14,
+        title: 'Creatine Integral Médica',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/creatina integral medica.png'
+    },
+
+    {
+        id: 15,
+        title: 'Pré-Treino No Control',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/no_control_-.png'
+    },
+
+    {
+        id: 16,
+        title: 'Zma Black Skull',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/ZMA-BLACK-SKULL-120-CAPS.png'
+    },
+
+    {
+        id: 17,
+        title: 'Ripzzz Black Skull',
+        price: 89.90,
+        poster: '/docs/Tela_Principal/Imagens/ripzzz-triptofano-.png'
+    },
+    
     "Triptofano New Nutrition",
     "Complexo B New Nutrition",
     "Thermo cut HD Muscle Hd",
