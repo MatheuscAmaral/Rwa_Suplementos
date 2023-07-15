744c8c488087160480764d42f687c075b2294f52
Matheus Amaral <matheus.amaral.1443007@sga.pucminas.br>
2023-07-15 09:15:36 -0300

    ..

--- docs/Carrinho/script.js
+++ docs/Carrinho/script.js
@@ -1,3 +1,8 @@
+if (localStorage.getItem("token") == null) {
+    alert("Você precisa estar logado para acessar essa página");
+    window.location.href = "/docs/Login/index.html";
+  } 
+
 function goBack() {
     window.history.back();
 }
--- docs/endereco/script.js
+++ docs/endereco/script.js
@@ -34,3 +34,29 @@ function removeHidden() {
 }
 
 
+function enviarInfo() {
+  let ru = document.getElementById('rua').value;
+  let num = document.getElementById('number').value;
+  let bairr = document.getElementById('bairro').value;
+  let cid = document.getElementById('cidade').value;
+  let es = document.getElementById('estado').value;
+  let ce = document.getElementById('cep').value;
+
+  // Crie um elemento <span> para agrupar os dados
+  let enderecoSpan = document.createElement("span");
+
+  // Defina a classe CSS para o elemento <span>
+  enderecoSpan.classList.add("endereco");
+
+  // Defina o conteúdo do elemento <span> com os dados preenchidos
+  enderecoSpan.textContent = ` Endereço: ${ru} ${num}, ${bairr}, ${cid} - ${es}, ${ce}`;
+
+  // Acessar a div "alterarSenha" e adicionar o elemento <span>
+  let alterarSenhaDiv = document.getElementById("alterarSenha");
+  alterarSenhaDiv.innerHTML = ""; // Limpar qualquer conteúdo anterior
+
+  alterarSenhaDiv.appendChild(enderecoSpan);
+
+  let submitButton = document.getElementById("submit")
+  submitButton.addEventListener("click", addHidden())
+}
