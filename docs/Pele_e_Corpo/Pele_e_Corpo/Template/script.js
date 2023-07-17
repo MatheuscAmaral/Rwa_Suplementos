660b201c189035ead1996bbff05cdd60f8f22d01
Matheus <matheus.amaral.1443007@sga.pucminas.br>
2023-07-17 19:38:46 -0300

    details

--- docs/Pele_e_Corpo/Pele_e_Corpo/Template/script.js
+++ docs/Pele_e_Corpo/Pele_e_Corpo/Template/script.js
@@ -1,14 +1,18 @@
-if (localStorage.getItem("token") == null) {
-    alert("Você precisa estar logado para acessar essa página");
-    window.location.href = "/docs/Login/index.html";
-  }
-  
+if (localStorage.getItem("token") != null) {
   const userLogado = JSON.parse(localStorage.getItem("userLogado"));
   
   const logado = document.querySelector("#logado");
   logado.innerHTML = `Olá, ${userLogado.nome}`;
   
-  function sair() {
+}
+
+else  {
+  const icon = document.getElementById('icon')
+
+  icon.style.display = 'none'
+}
+
+function sair() {
     localStorage.removeItem("token");
     localStorage.removeItem("userLogado");
     window.location.href = "/Index.html";
--- docs/Whey/100 Whey Muscle Hd/script.js
+++ docs/Whey/100 Whey Muscle Hd/script.js
@@ -1,13 +1,15 @@
-if (localStorage.getItem("token") == null) {
-  alert("Você precisa estar logado para acessar essa página");
-  window.location.href = "/docs/Login/index.html";
-}
-
-const userLogado = JSON.parse(localStorage.getItem("userLogado"));
-
-const logado = document.querySelector("#logado");
-logado.innerHTML = `Olá, ${userLogado.nome}`;
-
+if (localStorage.getItem("token") != null) {
+  const userLogado = JSON.parse(localStorage.getItem("userLogado"));
+   
+   const logado = document.querySelector("#logado");
+   logado.innerHTML = `Olá, ${userLogado.nome}`;
+ }
+ 
+ else {
+   const icon = document.getElementById('icon')
+ 
+   icon.style.display = 'none'
+ }
 function sair() {
   localStorage.removeItem("token");
   localStorage.removeItem("userLogado");
