b3ebc2834438fccbe1b31e556028d8cdfec11ccc
Matheus <matheus.amaral.1443007@sga.pucminas.br>
2023-07-18 17:16:14 -0300

    .

--- assets/js/wheypromax.js
+++ assets/js/wheypromax.js
@@ -16,5 +16,5 @@ else {
 function sair() {
   localStorage.removeItem("token");
   localStorage.removeItem("userLogado");
-  window.location.href = "/Index.html";
+  window.location.href = "/index.html";
 }
